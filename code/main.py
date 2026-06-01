f53293c280cb228cfa9d42542203d5986cb2224f
Anskukisu <anton.harkila@gmail.com>
2026-06-01 16:03:46 +0300

    Update main.py
    Mentioned something :-)

--- code/main.py
+++ code/main.py
@@ -1,5 +1,6 @@
 from compiler import comp
 from assembler import assemble
+# In VS Code, this is the path, so that's why it won't work for you if you directly run this file.
 filename = "file.hmg"
 compiled = comp(filename)
 print(assemble(compiled))
